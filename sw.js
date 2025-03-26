e4bca7040b744d4e0f82af8bb8674e447178d8f7
GetyeTek <getyeteklu2@gmail.com>
2025-03-26 17:38:03 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,35 @@
-const CACHE_NAME = 'amharic-transliterator-v1';
+const CACHE_NAME = 'amharic-transliterator-v2'; // Changed version
 const urlsToCache = [
   '/',
   '/index.html',
-  '/styles.css',
+  '/style.css',  // Fixed filename
   '/script.js',
-  '/icon-192.png',
-  '/icon-512.png'
+  '/manifest.json',
+  'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap' // Added font
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
+      .then(response => response || fetch(event.request))
+  );
+});
+
+// Auto-update when new content is available
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      );
+    })
   );
 });
